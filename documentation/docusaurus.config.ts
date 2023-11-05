1f674a57ba7a2d0f2661f6b74aab8164965816fe
kiver.nikita <kiver.nikita@wb.ru>
2023-11-05 14:52:04 +0300

    fix doc 5

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -7,10 +7,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: process.env['DOCS_URL'] || 'http://example.com',
+  url: 'http://swarm-ingress-controller.swarm.svc.k8s.dldevel',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env['DOCS_BASE_URL'] || '/',
+  baseUrl: '/swarm_doc/',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
