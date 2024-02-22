7dc3208ac58f9b57ed3b70aa7f1c9b62e3505e7e
kiver.nikita <kiver.nikita@wb.ru>
2024-02-22 14:15:47 +0300

    add terraform dir

--- documentation/sidebars.ts
+++ documentation/sidebars.ts
@@ -1,5 +1,6 @@
 import { toNft } from './src/sidebars/technical-docs/to-nft'
 import { sgroups } from './src/sidebars/technical-docs/sgroups'
+import { terraform } from './src/sidebars/technical-docs/terraform'
 import { ruleConfiguration } from './src/sidebars/technical-docs/rule-configuration'
 
 const sidebars = {
@@ -41,6 +42,12 @@ const sidebars = {
     },
     {
       type: 'category',
+      label: 'Terraform',
+      collapsed: false,
+      items: terraform
+    },
+    {
+      type: 'category',
       label: 'Конфигурация  ресурсов',
       collapsed: false,
       link: {
@@ -53,12 +60,7 @@ const sidebars = {
       type: 'doc',
       label: 'Требования',
       id: 'tech-docs/installation-system-requirements',
-    },
-    {
-      type: 'doc',
-      label: 'Terraform провайдер',
-      id: 'tech-docs/installation-terraform',
-    },
+    }
   ]
 }
 
